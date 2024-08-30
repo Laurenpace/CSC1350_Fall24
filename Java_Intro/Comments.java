82afa6c0f853ed06d79b02980bb282bf5d867b5a
Lauren Pace <laurenpace99@gmail.com>
2024-08-30 11:17:25 -0500

    8/30

---
+++ Java_Intro/Comments.java
@@ -0,0 +1,18 @@
+package CSC1350_Fall24.Java_Intro;
+
+/**
+ * you can do program documentation here
+ * this can be exported to an html document
+ */
+public class Comments
+{
+    public static void main(String[] args) {
+        //this is a single line comment
+        //System.out.print("Comment out one line of code!");
+
+        /*
+        System.out.print("Comment out multiple lines of code!");
+        System.out.print("Another!")
+        */
+    }
+}

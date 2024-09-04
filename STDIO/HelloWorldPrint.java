1508af1ce954195534b3fadb93f082e853fe6c98
Lauren Pace <laurenpace99@gmail.com>
2024-09-04 11:14:52 -0500

    9/4

---
+++ STDIO/HelloWorldPrint.java
@@ -0,0 +1,8 @@
+package CSC1350_Fall24.STDIO;
+
+public class HelloWorldPrint {
+    public static void main(String[] args){
+        System.out.print("Hello");
+        System.out.print("World");
+    }
+}
---
+++ STDIO/HelloWorldPrintLn.java
@@ -0,0 +1,8 @@
+package CSC1350_Fall24.STDIO;
+
+public class HelloWorldPrintLn {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("World");
+    }
+}

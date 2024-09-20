a5baccc3d3dfdffec08f77df5c98dc450e646afb
Laurenpace <lauren.pace-2@selu.edu>
2024-09-20 09:41:50 -0500

    9/20

---
+++ Operators/AugmentedAssignmentOperators.java
@@ -0,0 +1,21 @@
+package CSC1350_Fall24.Operators;
+
+public class CompoundOperators {
+    public static void main(String[] args) {
+        int x = 5;
+        x += 1;
+        System.out.println(x);
+
+        int y = 5;
+        x -= 3;
+        System.out.println(x);
+
+        int z = 10;
+        z *= 2;
+        System.out.println(z);
+
+        int a = 8;
+        a /= 2;
+        System.out.println(a);
+    }
+}

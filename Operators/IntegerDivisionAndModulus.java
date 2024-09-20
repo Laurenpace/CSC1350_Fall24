8e38723eb56cc47570d2faa8a7ebb5639f9bd30b
Laurenpace <lauren.pace-2@selu.edu>
2024-09-19 22:36:16 -0500

    9/20

---
+++ Operators/IntegerDivisionAndModulus.java
@@ -0,0 +1,19 @@
+package CSC1350_Fall24.Operators;
+
+public class IntegerDivisionAndModulus {
+    public static void main(String[] args) {
+
+        int divideInts = 10 / 3;
+        System.out.println(divideInts);
+
+        int modulo = 10 % 3;
+        System.out.println(modulo);
+
+
+        double divideIntsToDouble = 10 / 3;
+        System.out.println(divideIntsToDouble);
+
+        double divideDoubles = 10.0 / 3.0;
+        System.out.println(divideDoubles);
+    }
+}
---
+++ Operators/MathLibrary.java
@@ -0,0 +1,9 @@
+package CSC1350_Fall24.Operators;
+
+public class MathLibrary {
+    public static void main(String[] args) {
+        System.out.println(Math.pow(2, 2));
+        double var = Math.abs(-4);
+        System.out.println(var);
+    }
+}

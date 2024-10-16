4e7d48186dc62e0ebd65cb8ae23dff6b653fa18a
Lauren Pace <laurenpace99@gmail.com>
2024-10-16 09:19:20 -0500

    updates

--- Loops/PromptingUntilValidEntry.java
+++ Loops/PromptingUntilValidEntry.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class PromptingUntilValidEntry {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        boolean valid = false;
+        boolean valid = true;
         double input;
         //just one way of doing this
-        while (!valid){
+        while (valid){
 
             System.out.print("Please enter a positive value < 100: ");
             input = in.nextDouble();
             if (0 <= input && input < 100) {
-                valid = true;
+                valid = false;
             }
             else {
                 System.out.println("Invalid input.");
--- Loops/SumFor.java
+++ Loops/SumFor.java
@@ -1,5 +1,22 @@
 package CSC1350_Fall24.Loops;
+/*
+input: first line n, the number of integers to sum
+9
+3
+2
+4
+5
+6
+7
+8
+9
+10
+11
+14
 
+
+//notice extra values you do not want to sum
+ */
 import java.util.Scanner;
 
 public class SumFor {
@@ -8,11 +25,9 @@ public class SumFor {
         int n = in.nextInt();
         int sum = 0; //declare out here so we can print it after loop
 //        int i = 0;
-        for (int i = 0; i <= n; i++) {
-            sum = sum + i;
+        for (int i = 0; i < n; i++) {
+            sum += in.nextInt();
         }
         System.out.println("Sum = " + sum);
-//        System.out.println(number);
-        System.out.println(n);
     }
 }

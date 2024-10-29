e458e3da732ff0e0ef751c22f4161abeff32cd03
Lauren Pace <laurenpace99@gmail.com>
2024-10-29 13:30:18 -0500

    10/29

--- Arrays/ArrayBasics.java
+++ Arrays/ArrayBasics.java
@@ -12,47 +12,36 @@ public class ArrayBasics {
 
         int[] arrayName = new int[5000];
 
-//        int arrLength = in.nextInt();
-//        int[] difArr = new int[arrLength];
+        int arrLength = in.nextInt();
+        int[] difArr = new int[arrLength];
 
 
-//        System.out.print("enter size for array: ");
-//        int[] arr = new int[in.nextInt()];
-//        System.out.println(arr.length);
-//
-//        for (int i = 0; i < arr.length; i++) {
-//            arr[i] = in.nextInt();
-//        }
-//
-//        for (int i = 0; i < arr.length; i++) {
-//            System.out.println(arr[i] * 2);
-//        }
-//
-//        int n1 = arr[0] / 5;
+        System.out.print("enter size for array: ");
+        int[] arr = new int[in.nextInt()];
+        System.out.println(arr.length);
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] * 2);
+        }
+
 
         String[] s = {"banana", "apple", "ana"};
-        int indexOfBanana = 100;
+        int indexOfBanana = 100; //something beyond the index
 
         for (int i = 0; i < s.length; i++) {
-            if(s[i].equals("ana")){
+            if(s[i].equals("banana")){
                 System.out.println(i);
                 indexOfBanana = 0;
             }
         }
 
-        int index = findString(s, "asdf");
-        if(index == -1) System.out.println("never found");
+        System.out.println(indexOfBanana);
 
 
-    }
 
-    public static int findString(String[] s, String looking){
-        int index = -1;
-        for (int i = 0; i < s.length; i++) {
-            if(s[i].equals(looking)){
-                index = i;
-            }
-        }
-        return index;
     }
 }

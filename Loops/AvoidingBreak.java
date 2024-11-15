84bf609a461e8031a5c36868f7f7dd4a7d55f6b0
Lauren Pace <laurenpace99@gmail.com>
2024-11-15 12:42:12 -0600

    11/15

--- Loops/AvoidingBreak.java
+++ Loops/AvoidingBreak.java
@@ -1,2 +1,21 @@
-package CSC1350_Fall24.Loops;public class AvoidingBreak {
+package CSC1350_Fall24.Loops;
+
+import java.util.Scanner;
+
+public class AvoidingBreak {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        boolean flag = true;
+        for (int i = 0; i < 5 && flag; i++) {
+            for (int j = 0; j < 3 && flag; j++) {
+                if(in.nextInt() == -1){
+                    flag = false;
+                }
+            }
+
+            System.out.println(i);
+        }
+    }
+
+
 }
